4f19ed54546f01259e214d2419d1298e8497b2d0
Alex <alexbibig@gmail.com>
2020-10-09 15:00:36 +0300

    feat: compete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let configArr = bracketsConfig.map((el) =>
+        el.reduce((sum, cur) => sum + cur)
+    );
+    let n = -1;
+    while (str.length != n) {
+        n = str.length;
+        for (let i = 0; i < configArr.length; i++) {
+            str = str.replace(configArr[i], "");
+        }
+    }
+    return str.length == 0 ? true : false;
+};
